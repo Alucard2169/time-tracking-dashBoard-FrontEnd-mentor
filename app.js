fc715f9160705fba3cbfda838ce565405ead5c40
Alucard2169 <ashish10deepanshu@gmail.com>
2022-02-26 14:04:04 +0530

    update json

--- app.js
+++ app.js
@@ -1,55 +1,72 @@
 // defining variables
 
 const subCardContainer = document.querySelector('.container');
-const subContainer = document.querySelector('.sub-card-container');
-const subCardBack = document.createElement('div');
-const backLogo = document.createElement('img');
-const subCard = document.createElement('div');
-const subCardUpperSection = document.createElement('section');
-const stuff = document.createElement('span');
-const menuLogo = document.createElement('img');
-const subCardLowerSection = document.createElement('div');
-const duration = document.createElement('h2');
-const history = document.createElement('p');
+
+
 
 let mode = 'weekly';
 
 
-// adding css Class to variables
-subCardBack.classList.add('subBack');
-backLogo.classList.add('loGo');
-subCardUpperSection.classList.add('upper');
-subCard.classList.add('sub-card');
-stuff.classList.add('thing');
-menuLogo.classList.add('logo');
-subCardLowerSection.classList.add('lower');
-duration.classList.add('duration');
-history.classList.add('history');
 
-// modifying the variables
+// getting data from data.json file
 
-stuff.innerText = 'Work';
-backLogo.src = 'images/icon-work.svg';
-backLogo.alt = 'work'
-menuLogo.src = 'images/icon-ellipsis.svg';
-duration.innerText = '32hrs';
-history.innerText = 'Last Week - 36hrs';
-
-//appending the variables(elements) to the sub-card
-subCardUpperSection.append(stuff, menuLogo);
-subCardLowerSection.append(duration, history);
-subCard.append(subCardUpperSection, subCardLowerSection)
-
-//appending sub card to the main card
-subCardBack.append(subCard, backLogo);
-subContainer.append(subCardBack);
-
-let data;
+let data; 
 
 async function getData() {
-    let res = await fetch('./data.json')
+    let res = await fetch('https://raw.githubusercontent.com/Alucard2169/time-tracking-dashBoard-FrontEnd-mentor/main/data.json')
 
-    return data = await res.json();
+    data = await res.json();
+    console.log(data)
+    
+    for (let i = 0; i < data.length; i++){
+
+        //create data elements
+
+        const subCardBack = document.createElement('div');
+        const backLogo = document.createElement('img');
+        const subCard = document.createElement('div');
+        const subCardUpperSection = document.createElement('section');
+        const stuff = document.createElement('span');
+        const menuLogo = document.createElement('img');
+        const subCardLowerSection = document.createElement('div');
+        const duration = document.createElement('h2');
+        const history = document.createElement('p');
+
+        // adding css Class to variables
+        subCardBack.classList.add('subBack');
+        backLogo.classList.add('loGo');
+        subCardUpperSection.classList.add('upper');
+        subCard.classList.add('sub-card');
+        stuff.classList.add('thing');
+        menuLogo.classList.add('logo');
+        subCardLowerSection.classList.add('lower');
+        duration.classList.add('duration');
+        history.classList.add('history');
+
+        
+        
+        //appending the variables(elements) to the sub-card
+        subCardUpperSection.append(stuff, menuLogo);
+        subCardLowerSection.append(duration, history);
+        subCard.append(subCardUpperSection, subCardLowerSection)
+        
+        //appending sub card to the main card
+        subCardBack.append(subCard, backLogo);
+        subCardContainer.append(subCardBack);
+        
+        // modifying the variables
+
+        stuff.innerText = data[i].title;
+        backLogo.src = data[i].image;
+        backLogo.alt = 'work';
+        menuLogo.src = 'images/icon-ellipsis.svg';
+        duration.innerText = '32hrs';
+        history.innerText = 'Last Week - 36hrs';
+        
+    }
+    
+
 }
 getData();
-console.log(data)
+
+
